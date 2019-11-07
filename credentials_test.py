4515e4221a4b7515782dc39111102500c13457f1
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-11-07 13:20:48 +0300

    credentials

--- credentials_test.py
+++ credentials_test.py
@@ -6,3 +6,12 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
 
         self.new_credentials = Credentials("Facebook", "user101", "user101@email", "QwerY23")
+
+    def test_init(self):
+        self.assertEqual(self.new_credentials.password, "QwerY23")
+        self.assertEqual(self.new_credentials.username, "user101")
+        self.assertEqual(self.new_credentials.platform, "Github")
+
+    def test_save_credential(self):
+        self.new_credential.save_credential()  # saving the new contact
+        self.assertEqual(len(Credentials.credentials_list), 1)
