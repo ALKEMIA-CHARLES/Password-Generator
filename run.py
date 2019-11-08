b87bd195d8025b05ca098ab6266b9d2045195fec
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-11-08 12:42:51 +0300

    DOCSTRINGS

--- run.py
+++ run.py
@@ -23,27 +23,35 @@ def create_credentials(username, password, email, platform):
 
 
 def save_credentials(credentials):
+    "This functions saves credentials"
     credentials.save_credentials()
 
 
 def delete_credentials(credentials):
+    "This function deletes credentials"
     credentials.delete_credentials()
 
 
 def look_for_credentials(platform):
+    "This function looks for credentials"
     return Credentials.find_by_platform(platform)
 
 
 def check_for_existing_credentials(platform):
+    "This function checks on already existing credentials"
     return Credentials.credentials_exists(platform)
 
 
 def display_credentials():
+    "This function works to display the credentials"
     return Credentials.display_credentials()
 
 
 def copy_password():
+    "This function copies the password"
     return Credentials.copy_password()
 
 def generate_password(length):
+    "This function generates the password while cheking the length at the same time"
     return Credentials.generate_password(length)
+
